301fc79832cb081c5239aee6b475daf7ac1e9eea
Rama2451655 <levakuramakrishnareddy4d2@gmail.com>
2023-11-11 08:39:06 +0530

    first commit

---
+++ apps4/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+def one(request):
+  return render(request,'one.html')
+def two(request):
+  return render(request,'two.html')
+def three(request):
+  return render(request,'three.html')
+def four(request):
+  return render(request,'four.html')
+
+# Create your views here.
